35ace714563bc6c6a7e299f64a7c7208ebc86e6a
Andrew Nowak <aacn500@users.noreply.github.com>
2016-07-19 11:38:16 +0100

    Uncommented Build

--- .travis/script.sh
+++ .travis/script.sh
@@ -6,9 +6,8 @@ unset PERL5LIB
 
 export PATH=/home/travis/.nvm/versions/node/v${TRAVIS_NODE_VERSION}/bin:$PATH
 
-# This install already happens in install.sh
-#cpanm --notest --installdeps . || find /home/travis/.cpanm/work -cmin -1 -name '*.log' -exec tail -n20  {} \;
-#perl Build.PL
-#./Build
+cpanm --notest --installdeps . || find /home/travis/.cpanm/work -cmin -1 -name '*.log' -exec tail -n20  {} \;
+perl Build.PL
+./Build
 
 ./Build test --verbose
