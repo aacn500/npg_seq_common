0e2621f76403836e375e93be7bf24b7b7886ea19
Andrew Nowak <aacn500@users.noreply.github.com>
2016-07-19 09:49:17 +0100

    Add travis functionality

---
+++ .travis/script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script was adapted from work by Keith James (keithj). The original source
+# can be found as part of the wtsi-npg/data_handling project here:
+#
+#   https://github.com/wtsi-npg/data_handling
+
+
+set -e -x
+
+unset PERL5LIB
+
+cpanm --notest --installdeps . || find /home/travis/.cpanm/work -cmin -1 -name '*.log' -exec tail -n20  {} \;
+perl Build.PL
+./Build
+
+./Build test --verbose
