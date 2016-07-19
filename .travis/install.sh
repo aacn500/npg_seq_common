ffd3056d8c5102d1c9bb0fe5cc3d8cff3a64efae
Andrew Nowak <aacn500@users.noreply.github.com>
2016-07-19 09:49:17 +0100

    Add travis functionality

---
+++ .travis/install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# This file was adapted from work by Keith James (keithj) and Jaime Tovar Corona
+# (jmtc). The original source can be found as part of the wtsi-npg/data_handling
+# and wtsi-npg/qc projects here:
+#
+#   https://github.com/wtsi-npg/data_handling
+#   https://github.com/wtsi-npg/npg_qc
+
+set -e -x
+
+sudo apt-get install libgd2-xpm-dev # For npg_tracking
+sudo apt-get install liblzma-dev # For npg_qc
+
+# CPAN as in npg_npg_deploy
+cpanm --notest --reinstall App::cpanminus
+cpanm --no-lwp --notest https://github.com/wtsi-npg/perl-dnap-utilities/releases/download/${DNAP_UTILITIES_VERSION}/WTSI-DNAP-Utilities-${DNAP_UTILITIES_VERSION}.tar.gz
+
+# WTSI NPG Perl repo dependencies
+cd /tmp
+git clone --branch devel --depth 1 https://github.com/wtsi-npg/ml_warehouse.git ml_warehouse.git
+git clone --branch devel --depth 1 https://github.com/wtsi-npg/npg_tracking.git npg_tracking.git
+git clone --branch devel --depth 1 https://github.com/wtsi-npg/npg_qc.git npg_qc.git
+
+
+repos="/tmp/ml_warehouse.git /tmp/npg_tracking.git /tmp/npg_qc.git"
+
+for repo in $repos
+do
+  cd "$repo"
+  cpanm --quiet --notest --installdeps . || find /home/travis/.cpanm/work -cmin -1 -name '*.log' -exec tail -n20  {} \;
+  perl Build.PL
+  ./Build
+  ./Build install
+done
+
+cd "$TRAVIS_BUILD_DIR"
