07c2aad9432e10cc81e50ec054d189c93622699b
Andrew Nowak <aacn500@users.noreply.github.com>
2016-07-19 10:19:20 +0100

    Removed duplicate build

--- .travis/script.sh
+++ .travis/script.sh
@@ -6,8 +6,9 @@ unset PERL5LIB
 
 export PATH=/home/travis/.nvm/versions/node/v${TRAVIS_NODE_VERSION}/bin:$PATH
 
-cpanm --notest --installdeps . || find /home/travis/.cpanm/work -cmin -1 -name '*.log' -exec tail -n20  {} \;
-perl Build.PL
-./Build
+# This install already happens in install.sh
+#cpanm --notest --installdeps . || find /home/travis/.cpanm/work -cmin -1 -name '*.log' -exec tail -n20  {} \;
+#perl Build.PL
+#./Build
 
 ./Build test --verbose
