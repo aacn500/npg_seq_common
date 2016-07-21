3360f8bb5e406fa4b183a80462ac6629d5d6dc40
Andrew Nowak <aacn500@users.noreply.github.com>
2016-07-21 12:28:58 +0100

    Point correctly to configure location

--- .travis/install.sh
+++ .travis/install.sh
@@ -40,7 +40,7 @@ pushd bowtie2
 make
 popd
 
-# samtools
+# samtools 0.1.19
 
 wget http://sourceforge.net/projects/samtools/files/samtools/0.1.19/samtools-0.1.19.tar.bz2/download -O samtools-0.1.19.tar.bz2
 tar jxf samtools-0.1.19.tar.bz2
@@ -53,7 +53,7 @@ popd
 
 wget http://sourceforge.net/projects/staden/files/io_lib/${STADEN_IO_LIB_VERSION}/io_lib-${STADEN_IO_LIB_VERSION}.tar.gz/download -O io_lib.tar.gz
 tar xzf io_lib.tar.gz
-./io_lib/configure
+./io_lib-${STADEN_IO_LIB_VERSION}/configure
 
 # pb_calibration # for calibration_pu
 
