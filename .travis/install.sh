283ad719d1e26f319a5a275e6024a6633bb4bd66
Andrew Nowak <aacn500@users.noreply.github.com>
2016-07-29 11:35:33 +0100

    Remove Moosexroles

--- .travis/install.sh
+++ .travis/install.sh
@@ -14,7 +14,7 @@ sudo apt-get install liblzma-dev # For npg_qc
 cpanm --notest --reinstall App::cpanminus
 #cpanm --quiet --notest --reinstall ExtUtils::ParseXS
 cpanm --quiet --notest --reinstall MooseX::Role::Parameterized
-cpanm --quiet --notest Alien::Tidyp
+#cpanm --quiet --notest Alien::Tidyp
 cpanm --no-lwp --notest https://github.com/wtsi-npg/perl-dnap-utilities/releases/download/${DNAP_UTILITIES_VERSION}/WTSI-DNAP-Utilities-${DNAP_UTILITIES_VERSION}.tar.gz
 
 # WTSI NPG Perl repo dependencies
